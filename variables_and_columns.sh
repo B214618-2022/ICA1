cca58775eecfcd6dd7606123948fd81bf8ce211e
Michael Beavitt <s1653324@bioinfmsc7.bio.ed.ac.uk>
2022-10-23 17:50:29 +0100

    using a slightly unorthodox solution to get a table of counts now

--- variables_and_columns.sh
+++ variables_and_columns.sh
@@ -12,24 +12,13 @@ TIME48=$(awk '$4 ~ "48" {print$1}' fastq/Tco.fqfiles | grep -oP "[0-9]{4,}")
 GENOMEBED=$"/localdisk/data/BPSM/ICA1/TriTrypDB-46_TcongolenseIL3000_2019.bed"
 
 
-echo -e $UNINDUCED '\n'
-echo -e $INDUCED '\n'
-echo -e $CLONE1 '\n'
-echo -e $CLONE2 '\n'
-echo -e $WT '\n'
-echo -e $TIME0 '\n'
-echo -e $TIME24 '\n'
-echo -e $TIME48 '\n'
+mkdir cutfiles
+for x in $UNINDUCED; do 
+   cut -f6 output${x} > cutfiles/cut${x}
 
-#touch expression_table
-#for x in $UNINDUCED; do 
-#   cut -f6 output${x} > tempcutfile
-#   paste expression_table tempcutfile
-#
-#done
+done
 
-
-
+paste cutfiles/* | column -s $'\t' -t > didthisreallywork
 
 #finding means - to use	later
 #awk '{a=int(($2+$3)/2); $2=a; print}' $TEMPCOL
