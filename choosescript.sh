15569d51c0c34bcc8e2d09333cb4d8ce056b0404
Michael Beavitt <s1653324@bioinfmsc7.bio.ed.ac.uk>
2022-10-24 10:13:42 +0100

    finalizing the choose script

--- choosescript.sh
+++ choosescript.sh
@@ -16,7 +16,7 @@ do
   echo "$choice added. Select another?"
 done
 
-# Write out each choice
+# Write out each choice, save to file, format file into column
 printf "You selected the following: "
 for choice in "${choices[@]}"
 do
@@ -24,6 +24,8 @@ do
 done > group1choices
 cat group1choices
 echo -e '\n\n'
+for x in $(cat group1choices); do echo -e $x; done > group1choicescol
+rm group1choices
 
 echo 'Group 2 parameter selection. Please note, selecting all of one type e.g. Clone1, Clone2 and WT, will effectively select the entire set'
 #selecting parameters
@@ -50,5 +52,7 @@ do
 done > group2choices
 cat group2choices
 echo -e '\n\n'
+for x in $(cat group2choices); do echo -e $x; done > group2choicescol
+rm group2choices
 
 exit 0
