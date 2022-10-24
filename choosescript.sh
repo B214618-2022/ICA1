67a2f55b38d0cfe79c1678679a1d3575cc69d6a6
Michael Beavitt <s1653324@bioinfmsc7.bio.ed.ac.uk>
2022-10-24 10:01:51 +0100

    refining the choosing script

--- choosescript.sh
+++ choosescript.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo 'Group parameter selection. Please note, selecting all of one type e.g. Clone1, Clone2 and WT, will effectively select the entire set'
+echo 'Group 1 parameter selection. Please note, selecting all of one type e.g. Clone1, Clone2 and WT, will effectively select the entire set'
 #selecting parameters
 PS3=$'\n\n Please select:'
 options=$(cat uniq_params) #reading from uniq_params file generated from Tco.fqfiles 
@@ -21,7 +21,34 @@ printf "You selected the following: "
 for choice in "${choices[@]}"
 do
   printf "%s " "$choice"
+done > group1choices
+cat group1choices
+echo -e '\n\n'
+
+echo 'Group 2 parameter selection. Please note, selecting all of one type e.g. Clone1, Clone2 and WT, will effectively select the entire set'
+#selecting parameters
+PS3=$'\n\n Please select:'
+options=$(cat uniq_params) #reading from uniq_params file generated from Tco.fqfiles
+
+#array for parameters
+choices=()
+
+select choice in $options Finished
+do
+  # Stop choosing
+  [[ $choice = Finished ]] && break
+  # Append the choice to the array
+  choices+=( "$choice" )
+  echo "$choice added. Select another?"
 done
-printf '\n'
+
+# Write out each choice
+printf "You selected the following: "
+for choice in "${choices[@]}"
+do
+  printf "%s " "$choice"
+done > group2choices
+cat group2choices
+echo -e '\n\n'
 
 exit 0
